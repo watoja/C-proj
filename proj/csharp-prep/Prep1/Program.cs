5a1f5307db50b366c97906002ff4f5771be681f3
watoja <jimwattsabraham@gmail.com>
2023-09-23 19:45:54 +0300

    inti git

---
+++ proj/csharp-prep/Prep1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your first name");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name");
+        string last = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {first}, {first}  {last}. ");
+    }
+}
---
+++ proj/csharp-prep/Prep3/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user for the magic number.
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // Ask the user for a guess.
+        int guess = int.Parse(Console.ReadLine());
+
+        // Keep guessing until the user gets it right.
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            // Get the user's next guess.
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        // Congratulations!
+        Console.WriteLine("Congratulations! ");
+    }
+}
