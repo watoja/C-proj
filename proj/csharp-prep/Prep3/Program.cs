8177c3bb62796524fe23f706a6082b2a0f2ba236
watoja <jimwattsabraham@gmail.com>
2023-09-26 02:15:24 +0300

    prep3

--- proj/csharp-prep/Prep3/Program.cs
+++ proj/csharp-prep/Prep3/Program.cs
@@ -5,14 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Ask the user for the magic number.
-        Console.Write(" What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        //Console.Write(" What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
 
         // Ask the user for a guess.
-        Console.Write("Make a guess..:");
+        //Console.Write("What is your guess? ");
 
-        int guess = int.Parse(Console.ReadLine());
+        //int guess = int.Parse(Console.ReadLine());
 
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
         // Keep guessing until the user gets it right.
         while (guess != magicNumber)
         {
@@ -26,6 +30,7 @@ class Program
             }
 
             // Get the user's next guess.
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
         }
 
