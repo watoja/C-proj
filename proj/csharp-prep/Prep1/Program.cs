b12e107d6259df5647a5763b35f9d59d6c4966fa
watoja <jimwattsabraham@gmail.com>
2023-10-05 18:22:26 +0300

    prep5

--- proj/csharp-prep/Prep1/Program.cs
+++ proj/csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Console.Write("What is your first name?");
         string first = Console.ReadLine();
 
--- proj/csharp-prep/Prep5/Program.cs
+++ proj/csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[]args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName =PromptUserName();
+        int userNumber =PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
     }
-}
+    static void DisplayWelcomeMessage()
+    {
+        //welcome notice
+        Console.WriteLine("welcome!");
+    }
+
+    static string PromptUserName()
+    {
+        //asking for user name
+        Console.Write("My name is:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        //inputting user number
+        Console.Write("Enter user Number:");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    
+        static int SquareNumber(int number)
+        {
+            //squaring
+            int square = number*number;
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, The Square of your number is {square}.");
+        }
+
+    }
+
